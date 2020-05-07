8281d43ed7133f35119db0b7d3946aa2b21f4cc5
BouncyHunter <h168890687@outlook.com>
2020-05-07 17:42:20 +0800

    Fixed a bug that may cause the timer notice the last request only.\nNow outdated requests will be removed automatically.\nAdded a new command 'tick [seconds]' to change for a better cooldown of the timer.

--- autobook.py
+++ autobook.py
@@ -13,11 +13,14 @@ data_book = {'data_type': 'seatDate', 'seatno': '2-032', 'seatdate': '2000-04-24
 urls = {'UserInfo': 'http://wx.lib.hunnu.edu.cn/mobile/ajax/user/UserHandler.ashx'}
 data_personal = {'data_type': 'user_info'}
 booklist = []
+converted_date = []
 time_avai = datetime.time(6, 59, 50, 0)
 time_forb = datetime.time(23, 0, 0, 0)
+tick = 0.5
+ima = datetime.datetime.now()
 
 
-class MainThread (threading.Thread):   #继承父类threading.Thread
+class MainThread (threading.Thread):
     global stopp
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
@@ -27,7 +30,10 @@ class MainThread (threading.Thread):   #继承父类threading.Thread
     def run(self):
         #print(booklist[self.threadID])
         #print(self.threadID)
-
+        if ima.__gt__(converted_date[self.threadID] - datetime.timedelta(minutes=30)):
+            del booklist[self.threadID]
+            del converted_date[self.threadID]
+            return
         try:
             test = requests.post('http://wx.lib.hunnu.edu.cn/mobile/ajax/seat/SeatDateHandler.ashx',
                                  data=booklist[self.threadID], cookies=cookies, headers=header_example)
@@ -41,12 +47,14 @@ class MainThread (threading.Thread):   #继承父类threading.Thread
                 print(booklist[self.threadID]),
                 print(' booked.')
                 del booklist[self.threadID]
+                del converted_date[self.threadID]
 
 
 
 
 
 def timer_main():
+    global ima
     ima = datetime.datetime.now()
     #print('\n=================================\n')
     #print(ima)
@@ -55,13 +63,18 @@ def timer_main():
         for i in range(0, len(booklist)):
             #print(i)
             MainThread(i, 'whatever', i).start()
-    main_timer = Timer(0.5, timer_main)
+    main_timer = Timer(tick, timer_main)
     main_timer.start()
 
 
-main_timer = Timer(0.5, timer_main)
-main_timer.start()
 
+
+cookies['ASP.NET_SessionId'] = input('ASP.NET_SessionId:')
+cookies['txw_cookie_txw_unit_Id'] = input('txw_cookie_txw_unit_Id:')
+cookies['dt_cookie_user_name_remember'] = input('dt_cookie_user_name_remember:')
+
+main_timer = Timer(tick, timer_main)
+main_timer.start()
 
 print('Checking...')
 try:
@@ -85,20 +98,25 @@ else:
         while comm_slice[0] != 'quit':
             if comm_slice[0] == 'book':
                 if len(comm_slice) >= 5:
-                    data_book['seatno'] = comm_slice[1]
-                    data_book['seatdate'] = comm_slice[2]
-                    data_book['datetime'] = [comm_slice[3], comm_slice[4]]
-                    booklist.append(data_book)
+                    booklist.append({'data_type': 'seatDate', 'seatno': comm_slice[1], 'seatdate': comm_slice[2],
+                                     'datetime': [comm_slice[3], comm_slice[4]]})
+                    converted_date.append(datetime.datetime.strptime(comm_slice[2], '%Y-%m-%d') +
+                                          datetime.timedelta(hours=int(int(comm_slice[3]) / 60),
+                                                             minutes=int(comm_slice[3]) % 60))
                 else:
                     print('Invalid input')
-                    print('BOOK [SEAT NO] [DATE] [START TIME] [END TIME]\t\t\t\t\t\t\t\tattempt to book chosen seat of chosen time')
             elif comm_slice[0] == 'ls':
                 for i in booklist:
+                    print(i)
+                for i in converted_date:
                     print(i)
             elif comm_slice[0] == 'del':
                 if len(comm_slice) >= 2:
                     if comm_slice[1] < len(booklist):
                         del booklist[comm_slice[1]]
+            elif comm_slice[0] == 'tick':
+                if len(comm_slice) >= 2:
+                    tick = float(comm_slice[1])
             comm = raw_input('>')
             comm_slice = []
             slice_mark = 0;
